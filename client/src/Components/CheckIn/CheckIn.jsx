c26f617304b3c47a06be990761cb46393cbb9913
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-08-08 18:31:41 +0200

    add CheckIn component and some styles, CheckIn will be the first component that user sees, they will enter their username and enter the chat

---
+++ client/src/Components/CheckIn/CheckIn.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function CheckIn() {
+    return (
+        <section>
+            <input type="text" name="username" id="username" placeholder='Enter your username'/>
+            <button>Check in</button>
+        </section>
+    )
+}
+
+export default CheckIn;
