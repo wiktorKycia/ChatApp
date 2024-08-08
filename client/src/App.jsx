84fe918f0d501d135b624891f85199cd8094d029
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-08-08 18:58:28 +0200

    add some styles to CheckIn in order for the form to look normal (it was too big), center the input and the button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
-
+import ChatBox from "./Components/ChatBox/ChatBox.jsx"
+import CheckIn from "./Components/CheckIn/CheckIn.jsx"
 
 function App() {
-
+    return (
+        <main>
+            <CheckIn />
+        </main>
+    )
 }
 
 export default App
