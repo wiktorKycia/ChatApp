251b8ccb8f54f9020e72d472c26073d59c9988a9
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-08-09 18:44:15 +0200

    in App, create a context, that will send isCheckedIn and userName variables to CheckIn component; in the component, read the values, by a button, check if the input value is not empty, then change the user's name and check them in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,22 @@ import ChatBox from "./Components/ChatBox/ChatBox.jsx"
 import CheckIn from "./Components/CheckIn/CheckIn.jsx"
 import Message from "./Components/Message/Message.jsx";
 
-import React, {useState} from 'react';
+import React, {useState, createContext} from 'react';
+
+export const CheckContext = createContext();
 
 function App() {
-    const [isCheckedIn, setIsCheckedIn] = useState(true);
+    const [isCheckedIn, setIsCheckedIn] = useState(false);
+    const [userName, setUserName] = useState("");
     return (
         <>
+            <div>{userName}</div>
             {isCheckedIn ? <ChatBox>
                 <Message date={new Date().toLocaleTimeString()} text={"lorem ipsum dolor sit amet consectetur adipiscing elit suscipit facilisis hendrerit sagittis suscipit hendrerit hendrerit hendrerit hendrerit"} author={"author"}/>
                 <Message date={new Date().toLocaleTimeString()} text={"lorem"}/>
                 <Message date={new Date().toLocaleTimeString()} author={"yurmom"}/>
                 <Message date={new Date().toLocaleTimeString()} text={"stop posting shit \n please"} author={"admin"}/>
-            </ChatBox> :  <CheckIn />}
+            </ChatBox> : <CheckContext.Provider value={[setIsCheckedIn, setUserName]}><CheckIn /></CheckContext.Provider>}
         </>
     )
 }
--- client/src/Components/CheckIn/CheckIn.jsx
+++ client/src/Components/CheckIn/CheckIn.jsx
@@ -1,11 +1,34 @@
 import styles from './CheckIn.module.css'
-import React from 'react'
+import React, {useContext} from 'react'
+import {CheckContext} from '../../App.jsx'
+
 
 function CheckIn() {
+    const contextValues = useContext(CheckContext);
+    const setIsCheckedIn = contextValues[0];
+    const setUserName = contextValues[1];
+
+
+    function check()
+    {
+        let username = document.getElementById("username");
+        let user = username.value;
+        if (user === "")
+        {
+            alert("Please enter your username");
+        }
+        else
+        {
+            setIsCheckedIn(true);
+            setUserName(user);
+        }
+    }
+
+
     return (
         <section className={styles.container}>
             <input className={styles.input} type="text" name="username" id="username" placeholder='Enter your username'/>
-            <button className={styles.button}>Check in</button>
+            <button className={styles.button} onClick={check}>Check in</button>
         </section>
     )
 }
