6e06934376f2c4e2ac41b2701806adf57f6f5831
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-08-12 17:09:32 +0200

    in Message component, remove props, replace it with default js arguments, add one more: right=false, it indicates whether to show the message on the right or on the left

--- client/src/Components/Message/Message.jsx
+++ client/src/Components/Message/Message.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import styles from './Message.module.css'
 
-function Message(props) {
+function Message({date="could not load date", text="could not load message", author="unknown user", right=false}) {
     return (
         <div className={styles.message}>
-            <p className={styles.date}>{props.date}</p>
-            <p className={styles.text}>{props.text}</p>
-            <p className={styles.author}>{props.author}</p>
+            <p className={styles.date}>{date}</p>
+            <p className={styles.text}>{text}</p>
+            <p className={styles.author}>{author}</p>
         </div>
     )
 }
-Message.propTypes = {
-    text : PropTypes.string,
-    author : PropTypes.string,
-    date : PropTypes.string
-}
-Message.defaultProps = {
-    text : "could not load message",
-    author : "could not load author",
-    date : new Date().toLocaleTimeString()
-}
+// Message.propTypes = {
+//     text : PropTypes.string,
+//     author : PropTypes.string,
+//     date : PropTypes.string
+// }
+// Message.defaultProps = {
+//     text : "could not load message",
+//     author : "could not load author",
+//     date : new Date().toLocaleTimeString()
+// }
 export default Message
